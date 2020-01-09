9e86e512ff3cdd9ed3fb1b25a57cc5c0089c64ac
Baba <billard.baptiste.etudes@gmail.com>
2020-01-09 15:17:22 +0100

    Bug Fix (Electricity), DLC content
    - reducing wire size to fix elec bug
    - asteroid, level2 (content for DLC)

---
+++ Assets/Scripts/DLC/TheDLC.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Singleton
+public class TheDLC : MonoBehaviour
+{
+	private static TheDLC instance;
+
+	Scene level2;
+
+	private void Start()
+	{
+		if (!instance)
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+
+			ReadBundleContent();
+		}
+		else
+		{
+			Destroy(this);
+		}
+	}
+
+	void ReadBundleContent()
+	{
+		//if ( Application.persistentDataPath)
+		{
+
+		}
+	}
+}
